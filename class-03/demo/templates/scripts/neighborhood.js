1dee34859a6eb5b2d756de4d3d336fb65d2a5a22
Stacey Teltser <stacey.teltser@gmail.com>
2020-04-01 16:10:03 -0500

    mustache templating stuff

--- class-03/demo/templates/scripts/neighborhood.js
+++ class-03/demo/templates/scripts/neighborhood.js
@@ -1,26 +1,66 @@
 'use strict';
 
-// Identify the mustache template.
-// Why? The actual JS shouldn't have any 'knowledge' or 'insight' into the page structure at all.
-// We call this "separation of concerns"
-let templateId = '#neighborhood-template';
+const templateId = '#neighborhood-template';
+let templateHTML = $(templateId).html();
 
-let neighborhoods = [];
+let neighborhoodObject = {
+  name: 'NewBo',
+  city: 'Cedar Rapids',
+  population: '150k',
+  founded: '2020',
+  body: '<h1>Hi NewBo!</h1>'
+};
+let html = Mustache.render(templateHTML, neighborhoodObject);
 
-// REVIEW: This is another way to use a constructor to duplicate an array of raw data objects
-function Neighborhood(rawDataObject) {
-  for (let key in rawDataObject) {
-    this[key] = rawDataObject[key];
+console.log(html);
+
+$('#neighborhoods').append(html);
+
+
+
+function Neighborhood(data) {
+  for(let key in data) {
+    console.log(key);
+    this[key] = data[key];
+    //instead of this.name = data.name
   }
 }
 
-// Demo Part 1: Build it all with jQuery
-// Make sure and talk through each of these steps that needs to happen
-Neighborhood.prototype.toHtml = function () {
-  let container = $(`<div></div>`).clone();
-  container.append(`<h2>${this.name}</h2><p>Part of: ${this.city}</p>`);
-  return container;
-};
+Neighborhood.prototype.toHtml = function() {
+  let templateHTML = $(templateId).html();
+  let html = Mustache.render(templateHTML, this);
+  return html;
+}
+
+neighborhoodDataSet.forEach(neighborhoodData => {
+  let neighborhood = new Neighborhood(neighborhoodData);
+
+  console.log(neighborhood);
+  $('#neighborhoods').append(neighborhood.toHtml());
+})
+
+
+// Identify the mustache template.
+// Why? The actual JS shouldn't have any 'knowledge' or 'insight' into the page structure at all.
+// We call this "separation of concerns"
+// let templateId = '#neighborhood-template';
+
+// let neighborhoods = [];
+
+// // REVIEW: This is another way to use a constructor to duplicate an array of raw data objects
+// function Neighborhood(rawDataObject) {
+//   for (let key in rawDataObject) {
+//     this[key] = rawDataObject[key];
+//   }
+// }
+
+// // Demo Part 1: Build it all with jQuery
+// // Make sure and talk through each of these steps that needs to happen
+// Neighborhood.prototype.toHtml = function () {
+//   let container = $(`<div></div>`).clone();
+//   container.append(`<h2>${this.name}</h2><p>Part of: ${this.city}</p>`);
+//   return container;
+// };
 
 
 // Demo Part 2: Use jQuery to clone
@@ -45,10 +85,10 @@ Neighborhood.prototype.toHtml = function () {
 //   return html
 // };
 
-neighborhoodDataSet.forEach(neighborhoodObject => {
-  neighborhoods.push(new Neighborhood(neighborhoodObject));
-});
+// neighborhoodDataSet.forEach(neighborhoodObject => {
+//   neighborhoods.push(new Neighborhood(neighborhoodObject));
+// });
 
-neighborhoods.forEach(ourNewNeighborhoodObject => {
-  $('#neighborhoods').append(ourNewNeighborhoodObject.toHtml());
-});
+// neighborhoods.forEach(ourNewNeighborhoodObject => {
+//   $('#neighborhoods').append(ourNewNeighborhoodObject.toHtml());
+// });
