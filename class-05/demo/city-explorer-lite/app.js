f417a77cdece48c94273e4d2b8792ead77aff0be
Stacey Teltser <stacey.teltser@gmail.com>
2020-04-03 16:55:38 -0500

    city explorer working but deployment is broken

--- class-05/demo/city-explorer-lite/app.js
+++ class-05/demo/city-explorer-lite/app.js
@@ -1,7 +1,34 @@
 'use strict';
 
+
 // Weather data:
 // {
 //   "time": 1567792089082,
 //   "summary": "Foggy in the morning."
 // },
+
+function Weather(obj) {
+   this.time = new Date(obj.time);
+   this.summary = obj.summary;
+}
+
+Weather.prototype.render = function() {
+    let template = $('#weather-template').html();
+    let markUp = Mustache.render(template, this);
+    $('#weather-container').append(markUp);
+
+}
+const ajaxSettings = {
+    method: 'get',
+    dataType: 'json'
+}
+
+$.ajax('city-weather-data.json', ajaxSettings)
+.then(function (ajaxResult) {
+    console.log(ajaxResult);
+
+    ajaxResult.data.forEach(weatherData => {
+    let weather = new Weather(weatherData);
+    weather.render();
+    });
+})
--- class-05/demo/deployment/server.js
+++ class-05/demo/deployment/server.js
@@ -4,9 +4,9 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5504;
 
-app.use(express.static('./public'));
+// app.use(express.static('./public'));
 
 app.get('/hello', (request, response) => {
   response.status(200).send('Hello');
@@ -17,10 +17,14 @@ app.get('/data', (request, response) => {
     departure: Date.now(),
     canFly: true,
     pilot: 'Well Trained'
+  },
+  {
+    departure: new Date();
+    canFly: false,
   }
   response.status(200).json(airplanes);
 });
 
-app.use('*', (request, response) => response.send('Sorry, that route does not exist.'))
+// app.use('*', (request, response) => response.send('Sorry, that route does not exist.'))
 
 app.listen(PORT,() => console.log(`Listening on port ${PORT}`));
