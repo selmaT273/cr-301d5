793a2658410bbaba3356482e5b30acfec8187b8c
Keith Dahlby <keith@newbo.co>
2020-04-03 17:20:43 -0500

    Deployment demo

--- class-05/demo/deployment/server.js
+++ class-05/demo/deployment/server.js
@@ -1,26 +1,42 @@
 'use strict';
 
 require('dotenv').config();
+
+
+
+
+
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static('./public'));
 
+app.get('/heroku', (request, response) => {
+  response.status(200).send('Heroku is neat')
+})
+
 app.get('/hello', (request, response) => {
-  response.status(200).send('Hello');
+  console.log(request.method, request.path);
+  response.status(200).send(`Hello ${new Date()}`);
 });
 
 app.get('/data', (request, response) => {
-  let airplanes = {
-    departure: Date.now(),
-    canFly: true,
-    pilot: 'Well Trained'
-  }
+  let airplanes = [
+    {
+      departure: Date.now(),
+      canFly: true,
+      pilot: 'Well Trained'
+    },
+    {
+      departure: new Date(2020, 3, 7),
+      canFly: false,
+    }
+  ]
   response.status(200).json(airplanes);
 });
 
-app.use('*', (request, response) => response.send('Sorry, that route does not exist.'))
+// app.use('*', (request, response) => response.send('Sorry, that route does not exist.'))
 
 app.listen(PORT,() => console.log(`Listening on port ${PORT}`));
