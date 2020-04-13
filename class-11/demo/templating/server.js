75c1b39d8af5a103dbf82f4a40523e7f6ff2a9f4
Keith Dahlby <keith@newbo.co>
2020-04-13 15:57:46 -0500

    Add /list with some template replacement

--- class-11/demo/templating/server.js
+++ class-11/demo/templating/server.js
@@ -12,6 +12,20 @@ app.get('/', (request, response) =>{
   response.render('index');
 })
 
+app.get('/list', (request, response) => {
+  // TODO: go to DB
+  const list = ['apples','bacon','bacon','chocolate'];
+
+  const viewModel = {
+    groceryList: list,
+    user: {
+      name: 'Keith',
+      username: 'dahlbyk'
+    }
+  };
+  response.render('list', viewModel)
+})
+
 const cors = require('cors');
 app.use(cors());
 
