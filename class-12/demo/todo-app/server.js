a668608e08b0fb5d187ef8e2e644f8867807db93
Keith Dahlby <keith@newbo.co>
2020-04-14 16:03:18 -0500

    Fetch tasks from database

--- class-12/demo/todo-app/server.js
+++ class-12/demo/todo-app/server.js
@@ -8,6 +8,9 @@ const express = require('express');
 const pg = require('pg');
 
 // Database Setup
+if (!process.env.DATABASE_URL) {
+  throw 'DATABASE_URL is missing!';
+}
 const client = new pg.Client(process.env.DATABASE_URL);
 client.on('error', err => { throw err; });
 
@@ -18,18 +21,52 @@ const PORT = process.env.PORT || 3000;
 // Express middleware
 // Utilize ExpressJS functionality to parse the body of the request
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); // JSON body parser
+
 // Specify a directory for static resources
 app.use(express.static('./public'));
+
+// CORS would go here if we needed it
 
 // Set the view engine for server-side templating
 app.set('view engine', 'ejs');
 
 // API Routes
-
+app.get('/', getTasks);
 
 app.get('*', (req, res) => res.status(404).send('This route does not exist'));
 
-app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+client.connect()
+  .then(() => {
+    console.log('PG is listening!');
+
+    app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
+  })
+  .catch(err => { throw err; })
 
 
 // Route Handlers
+function getTasks(request, response) {
+  const SQL = 'SELECT * FROM Tasks;';
+
+  client.query(SQL)
+    .then(results => {
+      const { rowCount, rows } = results;
+      console.log('/ db result', rows);
+
+      // response.send(rows);
+      response.render('index', {
+        tasks: rows
+      });
+    })
+    .catch(err => {
+      handleError(err, response);
+    });
+}
+
+function handleError(err, response) {
+  let viewModel = {
+    error: err,
+  };
+  response.render('pages/error-view', viewModel);
+}
